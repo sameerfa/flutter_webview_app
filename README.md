3ea3cc690e2ecbbbf77d8d3bfef71b98d95c0b9f
Sameer FA <samervj@gmail.com>
2021-10-19 20:11:42 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# flutter_webview_app
+
+Boilerplate code for building Webview based Applications using Flutter. 
+
+Replace URL on line 49 inside lib/main.dart file and you're good to go.
+
+```
+cd flutter_webview_app
+flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+// @dart=2.9
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+  await Permission.microphone.request();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: InAppWebViewPage());
+  }
+}
+
+class InAppWebViewPage extends StatefulWidget {
+  @override
+  _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+}
+
+class _InAppWebViewPageState extends State<InAppWebViewPage> {
+  InAppWebViewController _webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        child: Container(
+          color: Colors.redAccent,
+        ),
+        preferredSize: Size.fromHeight(0.0),
+      ),
+      body: Container(
+        child: Column(children: [
+          Expanded(
+            child: InAppWebView(
+                initialUrl: "https://gmail.com",
+                initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions(
+                    mediaPlaybackRequiresUserGesture: false,
+                    debuggingEnabled: true,
+                  ),
+                ),
+                onWebViewCreated: (InAppWebViewController controller) {
+                  _webViewController = controller;
+                },
+                androidOnPermissionRequest: (InAppWebViewController controller,
+                    String origin, List<String> resources) async {
+                  return PermissionRequestResponse(
+                      resources: resources,
+                      action: PermissionRequestResponseAction.GRANT);
+                }),
+          ),
+        ]),
+      ),
+    );
+  }
+}
